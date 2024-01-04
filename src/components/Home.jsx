dc8960ab43949eb66416902b5608de2814e01b48
ankita_yadav87 <ankuy086@gmail.com>
2024-01-05 00:16:48 +0530

    home layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './home.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className='my_home'>
+      <div className='content'>
+        <h1>Welcome to Our Online Store</h1>
+        <p>Discover the latest trends and shop a wide range of products for the whole family.</p>
+        <p>Explore our collection of Clothing for Men and Women, Kids' Fashion, Stylish Accessories</p>
+        <p>Find the perfect style that suits you. Happy shopping!</p>
+         
+          <Link to="/products" type='button' className='my_button'>View Products</Link>
+    
+       
+        
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,42 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Badge from '@mui/material/Badge';
-
+import './home.css';
 
 const Navbar = () => {
-
   const state = useSelector((state) => state.handleCart.ttt);
-  console.log(state);
 
-  
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark  " style={{ height: "70px" }}>
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="#">ShopMart</Link>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ height: "70px" }}>
+      <div className=" d-flex justify-content-between align-items-center">
+        <div className="navbarList">
+          <Link className="navbar-brand navbarList" to="/">ShopMart</Link>
+        </div>
+
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          <ul className="navbar-nav justify-content-around align-items-center w-100">
+            <li className="nav-item navbarList">
+              <Link className="nav-link my_text" aria-current="page" to="/">Home</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/products">Products</Link>
+            <li className="nav-item navbarList">
+              <Link className="nav-link my_text" to="/products">Products</Link>
             </li>
           </ul>
-          {/* <Link to="/cart" className='btn btn-outline-dark ms-2'>
-          <i className="fa fa-shopping-cart">Cart({state.length})</i>
-        </Link> */}
 
           <Link to="/cart">
-          <Badge badgeContent={state.length} color="primary" className="mx-4" >
-          <i className="fa-solid fa-cart-shopping text-light p-2 " style={{ fontSize: 25, cursor: "pointer" }} ></i>
-        </Badge>
-        </Link>
-
-        
+            <Badge badgeContent={state.length} color="primary" className="mx-4">
+              <i className="fa-solid fa-cart-shopping text-light p-2 " style={{ fontSize: 25, cursor: "pointer" }} ></i>
+            </Badge>
+          </Link>
+        </div>
       </div>
-    </div>
-</nav >
-  )
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
